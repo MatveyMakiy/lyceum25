38552f88f273e9a95bf04d1c47bbfa197c2cc3f5
MatveyMakiy <mmakiy@yandex.ru>
2026-05-11 18:53:44 +0300

    Updated database on working laptop

---
+++ web/min/backend/prisma/migrations/20260511155313_add_likes/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "Like" (
+    "userId" TEXT NOT NULL,
+    "postId" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    PRIMARY KEY ("userId", "postId"),
+    CONSTRAINT "Like_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT "Like_postId_fkey" FOREIGN KEY ("postId") REFERENCES "Post" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
